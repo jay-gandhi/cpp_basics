9f297eb6d5f0cbae2e7fe71a091d18f9ad373f62
gjay7 <jay@discoverdollar.com>
2016-07-31 11:05:17 +0530

    Added HelloWorld program

---
+++ 01_cpp_basics/cpp_1_basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+using namespace std;
+
+This is very bad practice, Don't use this.
+*/
+int main() {
+  /* code */
+  std::cout << "First C++ Program" << std::endl;
+  /*
+    A << B
+    Insert B into A,
+    Right now, insert string into cout.
+  */
+  return 0;
+}
